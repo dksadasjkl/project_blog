357600907d0575a850879f413a05e463e3090866
dksadasjkl <ckdgus6038@naver.com>
2024-06-12 22:50:04 +0900

    feat : 회원가입 기능

--- src/main/java/com/study/home_project/controller/AuthController.java
+++ src/main/java/com/study/home_project/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.study.home_project.controller;
 
 import com.study.home_project.dto.AuthSignupRequestDto;
+import com.study.home_project.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthService authService;
+
+    // 회원가입
+    @PostMapping("/sign-up")
     public ResponseEntity<?> signup(@RequestBody AuthSignupRequestDto authSignupRequestDto) {
         System.out.println(authSignupRequestDto);
+        authService.signup(authSignupRequestDto);
         return ResponseEntity.created(null).body(true);
     }
 }
---
+++ src/main/java/com/study/home_project/service/AuthService.java
@@ -0,0 +1,12 @@
+package com.study.home_project.service;
+
+import com.study.home_project.dto.AuthSignupRequestDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    public void signup(AuthSignupRequestDto authSignupRequestDto) {
+
+    }
+}
